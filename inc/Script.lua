cbabe1fcf39a5e814d45d326e6d833281b32c545
TMLANA <83625300+TMLANA@users.noreply.github.com>
2021-05-29 02:08:57 +0300

    Update Script.lua

--- inc/Script.lua
+++ inc/Script.lua
@@ -3128,7 +3128,7 @@ local function iBoss(msg,MsgText)
     
     if (MsgText[1] == 'تحديث السورس' or MsgText[1] == 'تحديث السورس ™') then
     if not msg.SudoBase then return "📪¦ هذا الامر يخص {المطور الاساسي} فقط  \n" end
-    local GetVerison = https.request('https://wViiVw.github.io/GetVersion.txt') or "0"
+    local GetVerison = https.request('https://th3bs.github.io/GetVersion.txt') or "0"
     GetVerison = GetVerison:gsub("\n",""):gsub(" ","")
     if GetVerison > version then
     UpdateSourceStart = true
